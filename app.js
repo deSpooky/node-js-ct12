c1fdf9b5a9e249d94444490a6f046a27be8adef3
deSpooky <phurrde@gmail.com>
2025-05-21 21:31:09 +0300

    ejs

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+
+const app = express()
+
+app.set("view engine", "ejs")
+
+app.use("/contact", function(request, response) {
+    response.render("contact", {
+        title: "Мои контакты",
+        emailsVisible: true,
+        emails: ["spooky@mycorp.com", "purr@mycorp.com"],
+        phone: "+12345678910"
+    })
+})
+app.use("/", function(request, response) {
+    response.send("Главная страница")
+})
+
+app.listen(3000)
